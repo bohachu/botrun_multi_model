51d93808e0454244a6b51ae05fa0beccaa06862b
Aidoer <r05921078@ntu.edu.tw>
2024-05-14 16:37:25 +0800

    New: link to modal's website.

--- src/components/result/TabLabel.tsx
+++ src/components/result/TabLabel.tsx
@@ -2,6 +2,10 @@ export default function TabLabel({ name }: { name: string }) {
   return (
     <li className="tab-nav --active">
       <span>{name}</span>
+      <a href="https://www.youtube.com/" target="_blank">
+        <i className="icon icon-33"></i>
+        <span>瀏覽模型網站</span>
+      </a>
       <div className="close-tab-btn">
         <i></i>
       </div>
